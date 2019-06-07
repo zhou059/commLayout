a6b770e2333192c3d7a738d86f8c2ef843b378c9
jonathan-zhou <jonathan.zhou@salesforce.com>
2019-06-06 22:31:58 -0700

    initial layouts

---
+++ force-app/main/default/aura/customSearch/customSearchController.js
@@ -0,0 +1,15 @@
+({
+    handleClick : function(component, event, helper) {
+      var searchText = component.get('v.searchText');
+      var action = component.get('c.searchForIds');
+      action.setParams({searchText: searchText});
+      action.setCallback(this, function(response) {
+        var state = response.getState();
+        if (state === 'SUCCESS') {
+          var ids = response.getReturnValue();
+          console.log(ids);
+        }
+      });
+      $A.enqueueAction(action);
+    }
+})
